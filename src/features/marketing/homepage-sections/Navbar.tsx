58a254d041ef53452db82e00c8acb3ff2de18b53
Johnny Dong <dongjiayang123@gmail.com>
2026-07-07 19:35:01 -0700

    homepage button links to the start of membership perchasing wizard

--- src/features/marketing/homepage-sections/Navbar.tsx
+++ src/features/marketing/homepage-sections/Navbar.tsx
@@ -49,7 +49,7 @@ export default function Navbar() {
               Contact Us
             </Link>
 
-            <Button variant="primary" withArrow={false} shorterHeight={true} onClick={() => { window.location.href = "/portal/profile"; }}>
+            <Button variant="primary" withArrow={false} shorterHeight={true} onClick={() => { window.location.href = "/portal/membership/join"; }}>
               BECOME A MEMBER
             </Button>
           </div>
